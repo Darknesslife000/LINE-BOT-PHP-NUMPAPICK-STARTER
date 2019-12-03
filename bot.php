c9af38e4a1ac15f8081ea153bb569e184c09e021
DarkNessLifeMe <58324678+Darknesslife000@users.noreply.github.com>
2019-12-03 15:13:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,14 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
 	
+	$nub = strpos($events['ESP'], ',');
+	$msgindex1 = substr($events['ESP'],$nub+1,5);
+	$msgindex2 = substr($events['ESP'],$nub+7,8);
+	$msgcut = ",".$msgindex1.",".$msgindex2;
+	$events['ESP'] = str_replace($msgcut," ",$events['ESP']);
+
 	send_LINE($events['ESP']);
+	Sticker_LINE($msgindex1, $msgindex2);
 		
 	echo "OK";
 	}
