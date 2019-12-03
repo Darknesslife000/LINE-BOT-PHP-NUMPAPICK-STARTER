b67117a9a57e7b6d4bf8bed7e390acd1ca973102
DarkNessLifeMe <58324678+Darknesslife000@users.noreply.github.com>
2019-12-03 16:11:14 +0700

    Update line.php

--- line.php
+++ line.php
@@ -38,6 +38,12 @@ function send_LINE($msg1, $msg2, $msg3){
       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
       $result = curl_exec($ch);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post2);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
       curl_close($ch);
 
       echo $result . "\r\n"; 
